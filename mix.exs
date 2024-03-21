e17d746985f4531ed0d8609d7115841359336631
Wildan Fathan <wildan.nugraha@coinbit.id>
2024-03-21 23:46:11 +0700

    Add tools-version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NakamaForever.MixProject do
     [
       app: :nakama_forever,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
